5045951e3d074e4245a46775f2b75c8b59534fb2
Maxisokol <Maxisokol@users.noreply.github.com>
2014-08-14 15:24:51 +0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= 'Данные с датчиков', // Replaces Xively logo in the header
 		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor	= '880000', // CSS HEX value of color to represent data (omit leading #)
+		dataColor	= 'b00000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
